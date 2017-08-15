7883fbd3a8beba6fcde181426d84e7ff686c437a
MrPeach <767130703@qq.com>
2017-08-15 10:09:02 +0800

    commit 3:完成了根据类目类型查找类目的测试

--- src/test/java/com/lizhiyu/sell/repository/ProductCategoryRepositoryTest.java
+++ src/test/java/com/lizhiyu/sell/repository/ProductCategoryRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.lizhiyu.sell.repository;
 
 import com.lizhiyu.sell.dataobject.ProductCategory;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +9,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * Created by lzy on 17-8-13.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+/*@TransactionConfiguration(defaultRollback = false)*/
 public class ProductCategoryRepositoryTest {
 
 	@Autowired
@@ -22,7 +27,24 @@ public class ProductCategoryRepositoryTest {
 	@Test
 	@Transactional
 	public void findOneTest() {
-		ProductCategory productCategory = repository.getOne(2);
+		ProductCategory productCategory = repository.getOne(4);
 		System.out.println(productCategory.toString());
+	}
+
+	@Test
+	@Transactional
+	public void saveTest() {
+		ProductCategory productCategory = new ProductCategory("女生最爱",4);
+		ProductCategory result = repository.save(productCategory);
+		Assert.assertNotNull(result);
+		// Assert.assertNotEquals(null,result);
+	}
+
+	@Test
+	public void findByCategoryTypeIn() {
+		List<Integer> list = Arrays.asList(2,3,4);
+
+		List<ProductCategory> result = repository.findByCategoryTypeIn(list);
+		Assert.assertNotEquals(0,result.size());
 	}
 }
